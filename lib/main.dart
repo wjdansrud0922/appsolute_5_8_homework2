71f0e53895838e70c57d4a5dbd786b7e6853a153
wjdansrud0922 <wjdansrud092208@gmail.com>
2024-05-11 17:51:28 +0900

    NavigationBar

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+
+import 'package:flutter/material.dart';
+import 'package:instagram/screens/page1.dart';
+import 'package:instagram/screens/page2.dart';
+import 'package:instagram/screens/page3.dart';
+import 'package:instagram/screens/page4.dart';
+import 'package:instagram/screens/page5.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  var _selectedindex = 0;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Instagram"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.favorite),
+              onPressed: (){
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.message),
+              onPressed: (){
+              },
+            )
+          ],
+        ),
+        body: IndexedStack(
+          index: _selectedindex,
+          children: [
+            Page1(),
+            Page2(),
+            Page3(),
+            Page4(),
+            Page5(),
+          ],
+        ),
+        bottomNavigationBar: buildBottomNavigationBar(),
+      ),
+    );
+  }
+
+  BottomNavigationBar buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      currentIndex: _selectedindex,
+      onTap: (value) {
+        setState(() {
+          _selectedindex = value;
+        });
+        print("value : ${value}");
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: "home",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: "search",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add_box),
+          label: "add_box",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite),
+          label: "favorite",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.message),
+          label: "message",
+        ),
+      ],
+    );
+  }
+}
